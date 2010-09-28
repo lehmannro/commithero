b01ea5d27fadb8f77d057e165a692c63813e5644
Robert Lehmann <me@robertlehmann.de>
2010-09-28 17:19:54 +0200

    Added achievement for immediate reverts.

--- commithero/achievements/commits.py
+++ commithero/achievements/commits.py
@@ -1,3 +1,4 @@
+from . import Achievement
 from . import ProgressiveAchievement
 
 class Commits(ProgressiveAchievement):
@@ -21,3 +22,22 @@ class Merges(ProgressiveAchievement):
     def check(self, commit):
         return len(commit.parents) > 1 \
            and any(p.author != commit.author for p in commit.parents)
+
+class Ooopsie(Achievement):
+    "Immediately withdraw one of your changes again"
+    def on_commit(self, author, commit):
+        if len(commit.parents) != 1 or len(commit.parents[0].parents) != 1:
+            return
+        parent = commit.parents[0].parents[0]
+        for f in commit.get_changed_files():
+            print "change:", f
+            try:
+                old = parent.file_content(f)
+            except IOError:
+                old = ""
+            try:
+                new = commit.file_content(f)
+            except IOError:
+                new = ""
+            if old == new:
+                return True
