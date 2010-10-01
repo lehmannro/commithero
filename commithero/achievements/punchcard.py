da10276abea1298b0b0d009bbb367939a41ddf4d
Robert Lehmann <me@robertlehmann.de>
2010-10-01 10:38:20 +0200

    Added time travel achievement.

--- commithero/achievements/punchcard.py
+++ commithero/achievements/punchcard.py
@@ -4,3 +4,8 @@ class Insomniac(Achievement):
     "More coding, less sleeping."
     def on_commit(self, author, commit):
         return commit.time.hour in (2, 3, 4)
+
+class GrandfatherParadox(Achievement):
+    "Technically, you are your own parent now."
+    def on_commit(self, author, commit):
+        return any(commit.time < parent.time for parent in commit.parents)
