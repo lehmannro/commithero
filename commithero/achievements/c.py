22b476f06c7b99aa6c52841986e709882f24b0b7
Robert Lehmann <me@robertlehmann.de>
2010-10-04 11:52:09 +0200

    Overhauled C achievements.

--- commithero/achievements/c.py
+++ commithero/achievements/c.py
@@ -1,21 +1,23 @@
 from . import Achievement
 
-class ThreeStarProgrammer(Achievement):
-    "Cram at least three levels of indirection into your head."
-    def on_commit(self, author, commit):
-        diff = commit.get_parent_diff()
-        for line in diff:
-            if line.startswith('+'):
-                if 'void***' in line:
-                    return True
+class CAchievement(Achievement):
+    ext = [
+           'h', 'c', 'i' # C
+           'ii', 'cc', 'cp', 'cxx', 'cpp', 'c++', # C++
+           'hh', 'hp', 'hxx', 'hpp', 'h++', 'tcc', # C++ headers
+           'm', 'mi', # Objective-C
+           'xs', # Perl External Subroutine
+          ]
 
-class Win32Hell(Achievement):
+class ThreeStarProgrammer(CAchievement):
+    "Cram at least three levels of indirection into your head."
+    def on_change(self, old, new):
+        return new.count('void***') - old.count('void***') > 0
+
+class Win32Hell(CAchievement):
     "My life for Redmond!"
-    def on_commit(self, author, commit):
-        for path in commit.get_changed_files():
-            diff = commit.get_parent_diff()
-            if "\n+#ifdef _WIN32" in diff \
-            or "\n+#ifdef WIN32" in diff \
-            or "\n+#if defined(_WIN32)" in diff \
-            or "\n+#if defined(WIN32)" in diff:
-                return True
+    def on_change(self, old, new):
+        return sum(new.count(line) - old.count(line) for line in
+                   ("\n#ifdef _WIN32", "\n#ifdef WIN32",
+                    "\n#if defined(_WIN32)", "\n#if defined(WIN32)")
+               ) > 0
